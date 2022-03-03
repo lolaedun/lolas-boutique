0016b3915ee299eba6d832b84a35e11b8f9eaeb6
Lola <mledun@hotmail.com>
2022-03-03 20:16:25 +0000

    Added adjust and remove bag views and calc subtotal

--- products/models.py
+++ products/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Category(models.Model):
+
     class Meta:
         verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
 
     def get_friendly_name(self):
@@ -21,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
