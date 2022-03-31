bcd345e7d3d82df71620e443710739870b7d28ae
Lola <mledun@hotmail.com>
2022-03-31 20:48:16 +0000

    add staticfiles

--- lolas_boutique/settings.py
+++ lolas_boutique/settings.py
@@ -185,6 +185,7 @@ cloudinary.config(
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
