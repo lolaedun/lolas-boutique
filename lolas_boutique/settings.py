b5433bcd69a4003f683d5efcfaec492664c101fa
Lola <mledun@hotmail.com>
2022-04-01 22:25:56 +0000

    updated setting and requirements

--- lolas_boutique/settings.py
+++ lolas_boutique/settings.py
@@ -201,3 +201,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'mledun@hotmail.com'
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
--- products/admin.py
+++ products/admin.py
@@ -26,4 +26,3 @@ class CategoryAdmin (admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
-
