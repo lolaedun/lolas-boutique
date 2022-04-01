2dda78d591030933e9acdded62b4d14696366441
Lola <mledun@hotmail.com>
2022-04-01 21:09:39 +0000

    add static files

--- lolas_boutique/settings.py
+++ lolas_boutique/settings.py
@@ -184,14 +184,14 @@ if 'USE_CLOUDINARY' in os.environ:
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static-root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media-root')
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
