a12a0c35e572ff8eddaece78aaca2cf815ba9d34
Lola <mledun@hotmail.com>
2022-04-02 21:01:21 +0000

    added changes to static links

--- lolas_boutique/settings.py
+++ lolas_boutique/settings.py
@@ -182,7 +182,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'lolas_boutique/static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
