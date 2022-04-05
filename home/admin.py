37bfa8f693f699f05ec3bac53feaa64894a40f5e
Lola <mledun@hotmail.com>
2022-04-05 18:59:45 +0000

    created footer and models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Nav, Footer
 # Register your models here.
+
+admin.site.register(Nav)
+admin.site.register(Footer)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Nav (models.Model):
+    nav_title = models.CharField(max_length=100)
+    category_name = models.CharField(max_length=100)
+
+
+class Footer (models.Model):
+    bio = models.TextField(help_text='enter your bio or about us information here')
+    facebook = models.CharField(max_length=100)
+    linkedin = models.CharField(max_length=100)
+    github = models.CharField(max_length=100)
+    subscribe = models.EmailField(max_length=254)
